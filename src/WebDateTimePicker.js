48df7683a6e302eb54e3212dc1accd0743b482fd
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-01-04 19:55:54 -0300

    Document WebDateTimePicker

--- src/WebDateTimePicker.js
+++ src/WebDateTimePicker.js
@@ -56,78 +56,123 @@ export default function WebDateTimePicker(props) {
         }
         const date = new Date(dateString);
         if (date.getFullYear() > 999) {
-            props.setValue(date);
+            props.onChangeDate(date);
         }
     }
 
-    const inputStyle = { ...props.style };
+    const style = { ...props.style };
 
     return (
         <TextInput
-            {...props}
             ref={ref}
-            left={null}
-            right={null}
+            mode={props.mode}
+            disabled={props.disabled}
+            label={props.label}
+            error={props.error}
+            selectionColor={props.selectionColor}
+            underlineColor={props.underlineColor}
+            activeUnderlineColor={props.activeUnderlineColor}
+            outlineColor={props.outlineColor}
+            activeOutlineColor={props.activeOutlineColor}
+            dense={props.dense}
             multiline={false}
             numberOfLines={1}
             render={() => {
-                const fontSize = inputStyle.fontSize && 16;
-                const style = {
+                // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L102
+                // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L102
+                // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/constants.tsx#L1
+                const fontSize = style.fontSize && 16;
+                const renderStyle = {
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L416
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L453
                     flexGrow: 1,
                     margin: 0,
                     borderWidth: 0,
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L342
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L375
                     fontSize: fontSize,
                     fontFamily: theme.fonts.regular.fontFamily,
-                    fontWeight: inputStyle.fontWeight && theme.fonts.regular.fontWeight,
+                    fontWeight: style.fontWeight && theme.fonts.regular.fontWeight,
                     backgroundColor: 'transparent',
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L106
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L148
+                    // https://github.com/callstack/react-native-paper/blob/main/src/styles/DefaultTheme.tsx#L18
+                    // https://github.com/callstack/react-native-paper/blob/main/src/styles/DarkTheme.tsx#L20
                     color: props.disabled ? theme.colors.placeholder : theme.colors.text,
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L346
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L379
                     textAlignVertical: 'center',
-                    textAlign: inputStyle.textAlign && (I18nManager.isRTL ? 'right' : 'left'),
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L347
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L380
+                    textAlign: style.textAlign && (I18nManager.isRTL ? 'right' : 'left'),
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L353
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L386
                     outline: 'none',
                 };
                 if (props.mode === 'outlined') {
-                    style.zIndex = 1;
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L416
+                    renderStyle.zIndex = 1;
                     let padding;
-                    if (inputStyle.height) {
-                        style.height = inputStyle.height;
-                        padding = Math.max(0, (inputStyle.height - (inputStyle.lineHeight && fontSize)) / 2);
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L43
+                    if (style.height) {
+                        renderStyle.height = style.height;
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L115
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L111
+                        padding = Math.max(0, (style.height - (style.lineHeight && fontSize)) / 2);
                     } else {
-                        style.height = (props.dense ? 48 : 64) - 8;
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L147
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L42
+                        renderStyle.height = (props.dense ? 48 : 64) - 8;
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L141
                         padding = 0;
                     }
-                    style.paddingTop = padding;
-                    style.paddingBottom = padding;
-                    style.paddingLeft = 14;
-                    style.paddingRight = 14;
+                    renderStyle.paddingTop = padding;
+                    renderStyle.paddingBottom = padding;
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputOutlined.tsx#L45
+                    renderStyle.paddingLeft = 14;
+                    renderStyle.paddingRight = 14;
                 } else {
-                    if (inputStyle.height) {
-                        style.height = inputStyle.height;
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L43
+                    if (style.height) {
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L244
+                        renderStyle.height = style.height + 0;
                     } else {
-                        style.height = (props.dense ? (props.label ? 52 : 40) - 24 : 64 - 30);
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L186
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L244
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L44
+                        renderStyle.height = (props.dense ? (props.label ? 52 : 40) - 24 : 64 - 30) + (props.dense ? 24 : 30);
                     }
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L164
                     if (props.label) {
-                        style.paddingTop = props.dense ? 22 : 24;
-                        style.paddingBottom = props.dense ? 2 : 4;
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/TextInputFlat.tsx#L457
+                        renderStyle.paddingTop = props.dense ? 22 : 24;
+                        renderStyle.paddingBottom = props.dense ? 2 : 4;
                     } else {
-                        style.paddingTop = 0;
-                        style.paddingBottom = 0;
+                        // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/helpers.tsx#L169
+                        renderStyle.paddingTop = 0;
+                        renderStyle.paddingBottom = 0;
                     }
-                    style.paddingLeft = 12;
-                    style.paddingRight = 12;
+                    // https://github.com/callstack/react-native-paper/blob/main/src/components/TextInput/constants.tsx#L4
+                    renderStyle.paddingLeft = 12;
+                    renderStyle.paddingRight = 12;
                 }
                 return (
                     <input
-                        style={style}
-                        disabled={props.editable === false}
+                        type={props.type === 'time' ? 'time' : 'date'}
+                        disabled={props.disabled}
+                        onChange={onChange}
                         selectioncolor={props.selectionColor}
                         onFocus={onFocus}
                         onBlur={onBlur}
-                        type={props.type === 'time' ? 'time' : 'date'}
                         defaultValue={toString(props.value)}
-                        onChange={onChange}
+                        style={renderStyle}
+                        readOnly={props.editable === false}
                     />
                 );
             }}
+            style={style}
+            theme={props.theme}
+            editable={props.editable}
         />
     );
 }
